deaaa5c8636834168764b95ac05f0157cba759dd
Jack <jisapp02@gmail.com>
2020-01-18 23:05:13 -0500

    Light issue design

--- src/components/Issue.js
+++ src/components/Issue.js
@@ -3,6 +3,12 @@ import React from 'react';
 export function Issue (props) {
   return (
     <div className="issue">
+      <div className="issue-dragger">
+        <i className="fas fa-grip-horizontal"></i>
+      </div>
+      <div className="issue-avatar">
+        <img src={props.issue.user && props.issue.user.avatar_url} />
+      </div>
       {props.issue.title}
     </div>
   )
--- src/components/Repo.js
+++ src/components/Repo.js
@@ -4,10 +4,16 @@ import { setActiveRepo } from '../actions/repos'
 
 function Repo(props) {
   const onClick = () => { props.setActiveRepo(props.repo.name) }
-  
+  // TODO: Some indicator that a repo won't work. Why is bootcamp prep deprecated gone but others aren't?
+
   return (
     <div className="repo" onClick={onClick}>
-      {props.repo.name}
+      <div className="repo-name">
+        {props.repo.name}
+      </div>
+      <div className="repo-detail">
+        Open Issues: {props.repo.open_issues}
+      </div>
     </div>
   )
 }
