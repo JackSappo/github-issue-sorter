1a0ee4f4277d9b7cde59276ee7eca2143d03cbf1
Jack <jisapp02@gmail.com>
2020-01-18 22:14:14 -0500

    repo hover css

--- src/components/Repo.js
+++ src/components/Repo.js
@@ -6,7 +6,7 @@ function Repo(props) {
   const onClick = () => { props.setActiveRepo(props.repo.name) }
   
   return (
-    <div onClick={onClick}>
+    <div className="repo" onClick={onClick}>
       {props.repo.name}
     </div>
   )
