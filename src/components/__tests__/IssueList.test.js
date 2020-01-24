e32dca926e9d70af506d4338b791987e2730e043
Jack <jisapp02@gmail.com>
2020-01-24 00:38:43 -0500

    unit tests

--- src/components/__tests__/IssueList.test.js
+++ src/components/__tests__/IssueList.test.js
@@ -1,28 +1,51 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { IssueList } from '../IssueList'
-import { Issue } from '../Issue';
+import Issue from '../Issue';
 import { Loader } from '../Loader';
 
 describe('<IssueList />', () => {
-  it('should render expected element for null issues', () => {
+  [
+    {
+      issueCount: 0,
+      expectedEmpty: 1
+    },
+    {
+      issueCount: 1,
+      expectedEmpty: 0
+    },
+    {
+      issueCount: 5,
+      expectedEmpty: 0
+    },
+  ].forEach(({ issueCount, expectedEmpty }) => {
+    it(`should render expected elements for ${issueCount} issues`, () => {
+      const props = {
+        issues: new Array(issueCount).fill(undefined).map(_ => ({}))
+      }
 
-  })
+      const wrapper = shallow(
+        <IssueList {...props} />
+      );
 
-  it('should render expected element for 0 issues', () => {
-
-  })
-
-  it('should render expected element for 1 issues', () => {
-
-  })
-
-  it('should render expected element for 5 issues', () => {
-
+      expect(wrapper.find('EmptyView')).toHaveLength(expectedEmpty)
+      expect(wrapper.find(Issue)).toHaveLength(issueCount);
+    })
   })
 
   it('should render expected element when receiving error text', () => {
-    
+    const props = {
+      issues: [{}],
+      errorMessage: 'Some Error'
+    }
+
+    const wrapper = shallow(
+      <IssueList {...props} />
+    );
+
+    expect(wrapper.find('ErrorView')).toHaveLength(1);
+    expect(wrapper.find('EmptyView')).toHaveLength(0);
+    expect(wrapper.find(Issue)).toHaveLength(0);
   })
 
 });
