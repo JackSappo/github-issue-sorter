d22bbdb8fd9b79e52eaa6e8f4a7ec8f35d0b9ee1
Jack <jisapp02@gmail.com>
2020-01-23 23:03:27 -0500

    loading bugs

--- src/reducers/activeRepo.js
+++ src/reducers/activeRepo.js
@@ -2,7 +2,7 @@ export default (state = '', action) => {
   switch (action.type) {
     case 'SET_ACTIVE_REPO':
       return action.payload
-    case 'GET_REPOS':
+    case 'REPOS_LOADED':
       return '';
     default:
       return state;
